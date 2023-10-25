702918af86836feffdcf25e69d15c012c7f36cbd
Remi Collin <sharon.code@proton.me>
2023-10-25 18:12:40 +0200

    import data in Housing

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -6,6 +6,7 @@ function Housing() {
    const { id } = useParams();
    const { data, error, isLoading } = useFetch('../data/logements.json');
    const housingData = data[id];
+   const rating = [1, 2, 3, 4, 5];
 
    return (
       <main>
@@ -17,7 +18,41 @@ function Housing() {
                   <div>Loading...</div>
                ) : (
                   <div className="housingContainer">
-                     <img src={housingData.cover} alt={housingData.title} />
+                     <img
+                        className="cover"
+                        src={housingData.cover}
+                        alt={housingData.title}
+                     />
+                     <div className="info">
+                        <div className="title">{housingData.title}</div>
+                        <div className="location">{housingData.location}</div>
+                        <div className="host">{housingData.host.name}</div>
+                        <img
+                           className="hostCover"
+                           src={housingData.host.picture}
+                           alt={housingData.host.name}
+                        />
+                        <div className="tagContainer">
+                           {housingData.tags.map((tag) => (
+                              <div key={tag} className="tag">
+                                 {tag}
+                              </div>
+                           ))}
+                        </div>
+                        <div className="rating">
+                           {rating.map((rat) =>
+                              rat > housingData.rating ? '-' : '+'
+                           )}
+                        </div>
+                     </div>
+                     <div className="description">
+                        {housingData.description}
+                     </div>
+                     <div className="equipement">
+                        {housingData.equipments.map((equipments) => (
+                           <div key={equipments}>{equipments}</div>
+                        ))}
+                     </div>
                   </div>
                )}
             </div>
